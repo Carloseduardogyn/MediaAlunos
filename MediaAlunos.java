50ff2c5be3e92ce7e4732db0dc89e472df68e917
Carlos Eduardo <carloseduardogomeslima1@gmail.com>
2023-02-02 17:39:39 -0300

    Calcular a media dos alunos usando vetores

---
+++ MediaAlunos.java
@@ -0,0 +1,36 @@
+package vetores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MediaAlunos {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		String[] nomes = new String[n];
+		double[] nota1 = new double[n];
+		double[] nota2 = new double[n];
+
+		for (int i = 0; i < n; i++) {
+			nomes[i] = sc.next();
+			nota1[i] = sc.nextDouble();
+			nota2[i] = sc.nextDouble();
+		}
+
+		System.out.println("alunos aprovados: ");
+		for (int i = 0; i < n; i++) {
+			double media = (nota1[i] + nota2[i]) / 2.0;
+			if (media >= 6.0) {
+				System.out.println(nomes[i]);
+			}
+
+		}
+
+		sc.close();
+	}
+
+}
